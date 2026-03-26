1cab2517f699cc8c22c76dbc03473234d51e096a
JStack Innovations <wsamson630@gmail.com>
2026-03-26 10:10:02 +0100

    Update admins.php

--- api/admins/GET/admins.php
+++ api/admins/GET/admins.php
@@ -1,7 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET");
-header("Content-Type: application/json");
+require_once __DIR__ . "/../../SECURE/authGuard.php";
 
 
 $file = __DIR__ . '/../../SECURE/db.php';
