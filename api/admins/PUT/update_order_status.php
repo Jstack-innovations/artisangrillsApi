72935b74ad2d05c0ba87cec30a18fbafe1f2fcea
JStack Innovations <wsamson630@gmail.com>
2026-03-26 10:41:18 +0100

    Update update_order_status.php

--- api/admins/PUT/update_order_status.php
+++ api/admins/PUT/update_order_status.php
@@ -1,8 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: PUT, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Content-Type: application/json");
+require_once __DIR__ . "/../../SECURE/authGuard.php";
 
 require_once __DIR__ . '/../../SECURE/config.php';
 
