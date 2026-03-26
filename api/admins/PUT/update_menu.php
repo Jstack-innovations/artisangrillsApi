72a50a2b340210330ac1f56baa7d48bb4deb3f75
JStack Innovations <wsamson630@gmail.com>
2026-03-26 10:40:37 +0100

    Update update_menu.php

--- api/admins/PUT/update_menu.php
+++ api/admins/PUT/update_menu.php
@@ -1,16 +1,6 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: PUT, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Content-Type: application/json");
-
-if ($_SERVER['REQUEST_METHOD'] == "OPTIONS") {
-    http_response_code(200);
-    exit;
-}
-
-session_start();
+require_once __DIR__ . "/../../SECURE/authGuard.php";
 
 $menuFile = __DIR__ . "/../../GET/JSON/menu.json";
 
