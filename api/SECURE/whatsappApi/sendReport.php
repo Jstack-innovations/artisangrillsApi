b1a0290abb47fb11df29bb696880c40f36d68bfc
JStack Innovations <wsamson630@gmail.com>
2026-03-31 20:12:41 +0100

    Add CORS headers to sendReport.php

--- api/SECURE/whatsappApi/sendReport.php
+++ api/SECURE/whatsappApi/sendReport.php
@@ -1,5 +1,9 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
 require __DIR__ . '/vendor/autoload.php';
 
 use Twilio\Rest\Client;
