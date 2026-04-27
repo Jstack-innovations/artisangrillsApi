3e87ec341236c7d7b218d2fd625139816d9af8c8
JStack Innovations <wsamson630@gmail.com>
2026-04-27 06:37:43 +0100

    Replace gmail_mailer with resend_mailer in signup.php

--- api/POST/AUTH/signup.php
+++ api/POST/AUTH/signup.php
@@ -1,6 +1,9 @@
 <?php  
 
-require_once __DIR__ . '/../../SECURE/gmailApi/gmail_mailer.php';
+//require_once __DIR__ . '/../../SECURE/gmailApi/gmail_mailer.php';
+
+require_once __DIR__ . '/../../SECURE/gmailApi/resend_mailer.php';
+
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
