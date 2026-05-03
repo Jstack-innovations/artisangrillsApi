b88111427d4d2ec8ad8e3153e05d0b03c3fa104a
JStack Innovations <wsamson630@gmail.com>
2026-05-03 16:22:06 +0100

    Enable error reporting and CORS headers

--- api/POST/create-order.php
+++ api/POST/create-order.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
