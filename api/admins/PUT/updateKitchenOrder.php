2feed44b5cb32082c1fe252512cc4fc2a0c3e088
jStack●Innovations <wsamson630@gmail.com>
2026-05-29 06:43:29 +0100

    Update updateKitchenOrder.php

--- api/admins/PUT/updateKitchenOrder.php
+++ api/admins/PUT/updateKitchenOrder.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . "/../../SECURE/authGuard.php";
+//require_once __DIR__ . "/../../SECURE/authGuard.php";
 require_once __DIR__ . "/../../SECURE/db.php";
 
 $data = json_decode(file_get_contents("php://input"), true);
